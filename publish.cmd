ce4ad187869dbe68c205e16389456fd7de9b30bc
Jason Sowers <v-jassow@microsoft.com>
2018-01-19 10:01:11 -0800

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=jason-webappbot-Web-Deploy.pubxml -p:Password=XgQzc4jMKLqqdwxgB6t6BDnymv6gEYXhxGaPcf3HfTrRbAzEdEakd31qbfkj
+
